2ef86e04bc47402fe57a6eefaa3353d65fe5043b
rodolforezende <rodolforrcont@gmail.com>
2021-10-31 11:48:38 -0300

    Backend para login

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,19 +1,20 @@
 const User = require('../models/userSchema');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
-const { registerValidate } = require('../middlewares/validations');
+const { registerValidate, loginValidate } = require('../middlewares/validations');
 
-const userRegisters = async (name, email, password) => {
-  const validate = await registerValidate(name, email, password);
+const userRegisters = async (body) => {
+  const validate = await registerValidate(body);
   if (validate.message) {
     return validate;
   }
-  
-  const findExistUser = await User.findOne({ email });
-  if(findExistUser) return { message: 'Email already exists' };
+  const findExistUser = await User.findOne({ email: body.email });
+  console.log(findExistUser, 'data')
+  if (findExistUser) return { message: 'Email already exists' };
   const newUser = new User({
-    name,
-    email,
-    password: bcrypt.hashSync(password),
+    name: body.name,
+    email: body.email,
+    password: bcrypt.hashSync(body.password),
   });
   try {
     const savedUser = await newUser.save();
@@ -23,4 +24,19 @@ const userRegisters = async (name, email, password) => {
   }
 }
 
-module.exports = { userRegisters };
+const loginVerification = async (body) => {
+  const validate = await loginValidate(body.email, body.password);
+  if (validate.message) {
+    return validate;
+  }
+
+  const findExistUser = await User.findOne({ email: body.email });
+  if (!findExistUser) return { message: 'Email or Password incorrect' };
+
+  const passwordAndUserMatch = bcrypt.compareSync(body.password, findExistUser.password);
+  if (!passwordAndUserMatch) return { message: 'Email or Password incorrect' };
+
+  const token = jwt.sign({ _id: findExistUser._id, email: findExistUser,  admin: findExistUser.admin }, process.env.TOKEN_SECRET);
+  return token;
+}
+module.exports = { userRegisters, loginVerification };
